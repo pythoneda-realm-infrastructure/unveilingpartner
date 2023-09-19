4e91e3768ce553a0bf36cb1c0f64d45cc60d7884
rydnr <github@acm-sl.org>
2023-09-19 20:17:07 +0200

    Added dbus signal emitter and listener

---
+++ pythoneda/realm/unveilingpartner/infrastructure/dbus/unveilingpartner_dbus_signal_emitter.py
@@ -0,0 +1,59 @@
+"""
+pythoneda/realm/rydnr/infrastructure/dbus/rydnr_dbus_signal_emitter.py
+
+This file defines the RydnrDbusSignalEmitter class.
+
+Copyright (C) 2023-today rydnr's pythoneda-realm-rydnr/infrastructure
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from dbus_next import BusType
+from pythoneda.event import Event
+from pythoneda.infrastructure.dbus import DbusSignalEmitter
+from pythoneda.shared.artifact_changes.events import ChangeStaged
+from pythoneda.shared.artifact_changes.events.infrastructure.dbus import DbusChangeStaged
+from typing import Dict
+
+class UnveilingpartnerDbusSignalEmitter(DbusSignalEmitter):
+
+    """
+    A Port that emits UnveilingPartner events as d-bus signals.
+
+    Class name: UnveilingPartnerDbusSignalEmitter
+
+    Responsibilities:
+        - Connect to d-bus.
+        - Emit domain events as d-bus signals on behalf of Rydnr.
+
+    Collaborators:
+        - pythoneda.application.PythonEDA: Requests emitting events.
+        - pythoneda.shared.artifact_changes.events.infrastructure.dbus.DbusChangeStaged
+    """
+    def __init__(self):
+        """
+        Creates a new UnveilingpartnerDbusSignalEmitter instance.
+        """
+        super().__init__()
+
+    def signal_emitters(self) -> Dict:
+        """
+        Retrieves the configured event emitters.
+        :return: For each event, a list with the event interface and the bus type.
+        :rtype: Dict
+        """
+        result = {}
+        key = self.__class__.full_class_name(ChangeStaged)
+        result[key] = [ DbusChangeStaged, BusType.SYSTEM ]
+
+        return result
---
+++ pythoneda/realm/unveilingpartner/infrastructure/dbus/unveilingpartner_dbus_signal_listener.py
@@ -0,0 +1,63 @@
+"""
+pythoneda/realm/rydnr/infrastructure/dbus/rydnr_dbus_signal_listener.py
+
+This file defines the RydnrDbusSignalListener class.
+
+Copyright (C) 2023-today rydnr's pythoneda-realm-rydnr/infrastructure
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+from dbus_next import BusType, Message
+from pythoneda.event import Event
+from pythoneda.shared.artifact_changes.events import ChangeStagingCodeExecutionRequested
+from pythoneda.shared.artifact_changes.events.infrastructure.dbus import DbusChangeStagingCodeExecutionRequested
+from pythoneda.infrastructure.dbus import DbusSignalListener
+from typing import Dict
+
+class UnveilingpartnerDbusSignalListener(DbusSignalListener):
+
+    """
+    A Port that listens to Unveilingpartner-relevant d-bus signals.
+
+    Class name: UnveilingpartnerDbusSignalListener
+
+    Responsibilities:
+        - Connect to d-bus.
+        - Listen to signals relevant to UnveilingPartner.
+
+    Collaborators:
+        - pythoneda.application.pythoneda.PythonEDA: Receives relevant domain events.
+        - pythoneda.shared.artifact_changes.events.infrastructure.dbus.DbusChangeStagingCodeExecutionRequested
+    """
+
+    def __init__(self):
+        """
+        Creates a new UnveilingpartnerDbusSignalListener instance.
+        """
+        super().__init__()
+
+    def signal_receivers(self, app) -> Dict:
+        """
+        Retrieves the configured signal receivers.
+        :param app: The PythonEDA instance.
+        :type app: pythoneda.application.PythonEDA
+        :return: A dictionary with the signal name as key, and the tuple interface and bus type as the value.
+        :rtype: Dict
+        """
+        result = {}
+        key = self.__class__.full_class_name(ChangeStagingCodeExecutionRequested)
+        result[key] = [
+            DbusChangeStagingCodeExecutionRequested, BusType.SYSTEM
+        ]
+        return result
