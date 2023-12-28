469797e88538ce5821227b9aadab77fe7f504486
rydnr <github@acm-sl.org>
2023-12-28 16:06:27 +0100

    Various minor fixes

--- pythoneda/realm/unveilingpartner/infrastructure/dbus/unveilingpartner_dbus_signal_emitter.py
+++ pythoneda/realm/unveilingpartner/infrastructure/dbus/unveilingpartner_dbus_signal_emitter.py
@@ -19,7 +19,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from dbus_next import BusType
-from pythoneda.event import Event
 from pythoneda.infrastructure.dbus import DbusSignalEmitter
 from pythoneda.shared.artifact.events import ChangeStaged
 from pythoneda.shared.artifact.events.infrastructure.dbus import DbusChangeStaged
